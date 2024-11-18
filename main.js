07ae0a0865409754f2c16e32c7a327665d5d5dd0
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-11-19 00:01:35 +1300

    reset main.js execution is failing everytime for GET req

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-import { Client, Database } from "node-appwrite";
-
-const PROJECT_ID = process.env.PROJECT_ID;
-const COLLECTION_ID_PROFILES = process.env.COLLECTION_ID_PROFILES;
-const DB_ID = process.env.DB_ID;
-
 export default async ({ req, res, log, error }) => {
-  const client = new Client();
-  client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID);
-
-  const database = new Database(client);
-  if (req.method == "GET") {
-    //this is a GET request (when someone ask for a data ) to the server from the client
-    const response = await database.listDocuments(DB_ID, COLLECTION_ID_PROFILES); // the server response this request
-    return res.json(response);
-  }
   return res.send("Hello World");
 };
